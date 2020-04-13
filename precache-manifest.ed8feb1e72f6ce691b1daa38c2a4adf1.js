fcb7de48a39c10ef4bcf482da5b49594f658d1e9
Oliver <oliveroenang@gmail.com>
2020-04-13 10:54:45 +0200

    Updates

---
+++ precache-manifest.ed8feb1e72f6ce691b1daa38c2a4adf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23b18c9d83395ac4d1da98ff1878098",
+    "url": "/sxm-assignment/index.html"
+  },
+  {
+    "revision": "7a27847b1cd768a3bd40",
+    "url": "/sxm-assignment/static/js/2.25deb68f.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/sxm-assignment/static/js/2.25deb68f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d30dd8b31e166e85b07",
+    "url": "/sxm-assignment/static/js/main.e23a9187.chunk.js"
+  },
+  {
+    "revision": "aa982426d7ebd2c90db8",
+    "url": "/sxm-assignment/static/js/runtime-main.2362bdea.js"
+  },
+  {
+    "revision": "97df8ea30935266204922d9e90c35aa9",
+    "url": "/sxm-assignment/static/media/saxum.97df8ea3.jpg"
+  }
+]);
